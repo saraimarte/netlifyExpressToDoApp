58cfd9c51fb3ff1e4605a3d3ccef88f79b0c5489
Sarai Marte <smartepujols@gmail.com>
2025-02-02 21:32:38 -0500

    cors

--- src/api.js
+++ src/api.js
@@ -11,10 +11,13 @@ app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-
+    next();
 });
 
 app.use('/.netlify/functions/api/', router);
 
 module.exports.handler = serverless(app);
 
+//'https://backend200.netlify.app/.netlify/functions/api/' from origin
+// 'https://frontend200.netlify.app' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' 
+// header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
