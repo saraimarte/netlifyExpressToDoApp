819a73f98399292b700251dcec946c07c0676b79
Sarai Marte <smartepujols@gmail.com>
2025-02-02 21:11:56 -0500

    2

--- src/api.js
+++ src/api.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-
-const app = express();
-
 const serverless = require('serverless-http');
 
+const app = express();
 const router = require('./Routes/tasks.js');
 
-app.use(cors({ origin: 'https://frontend200.netlify.app' })); 
 
+app.use(cors()); 
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://frontend200.netlify.app'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use('/.netlify/functions/api/', router);
 
