98dc20489c12d54ae65e08b2936ed94ec36f1fe3
Sarai Marte <smartepujols@gmail.com>
2025-02-02 21:06:08 -0500

    added cors so frontend can make requests to here

--- src/api.js
+++ src/api.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 const serverless = require('serverless-http');
 
 const router = require('./Routes/tasks.js');
+
+app.use(cors({ origin: 'https://frontend200.netlify.app/*' })); 
+
 
 app.use('/.netlify/functions/api/', router);
 
